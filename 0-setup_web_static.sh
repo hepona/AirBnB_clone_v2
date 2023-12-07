a7a8619567bea350b1736f87b04304654fb841de
hepona <hepona10@gmail.com>
2023-12-07 16:11:40 +0100

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-sudo apt update
-sudo apt install nginx
-mkdir -p data
-mkdir -p data/web_static
-mkdir -p data/web_static/releases
-mkdir -p data/web_static/shared
-mkdir -p data/web_static/releases/test
-echo "Hello word toto" > /data/web_static/releases/index.html
-sudo ln -S -P /data/web_static/current /data/web_static/releases/test/
+# sudo apt update
+# sudo apt install nginx
+sudo mkdir -p data
+sudo mkdir -p data/web_static
+sudo mkdir -p data/web_static/releases
+sudo mkdir -p data/web_static/shared
+sudo mkdir -p data/web_static/releases/test
+echo "Hello word toto" >/data/web_static/releases/index.html
+sudo mkdir -p data/web_static/current
+sudo ln -sf data/web_static/releases/test/ data/web_static/current
 sudo chown ubuntu:ubuntu /data/
