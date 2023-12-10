2241373232559a28a2a71f0a344f88e2abadada9
hepona <hepona10@gmail.com>
2023-12-10 19:48:44 +0100

    1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,9 +11,10 @@ echo "Hello word toto" > /data/web_static/releases/index.html
 sudo mkdir -p /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-config_content="location /hbnb_static {\n\talias /data/web_static/current/;\n}\n"
-if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
-    sudo sed -i "/server_name _;/a $config_content" /etc/nginx/sites-available/default
-fi
+sudo sed -i "s@server_name _;@server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n@" /etc/nginx/sites-available/default
+# config_content="location /hbnb_static {\n\talias /data/web_static/current/;\n}\n"
+# if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
+#     sudo sed -i "/server_name _;/a $config_content" /etc/nginx/sites-available/default
+# fi
 sudo service nginx restart
 exit 0
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" Fabric script that generates a .tgz archive from the contents
+ of the web_static folder of your AirBnB Clone repo"""
+import os
+from fabric import task, local
+import datetime
+
+
+@task
+def do_pack():
+    """generate .tgz file"""
+    date = datetime.datetime.now()
+    filename = (
+        "web_static_",
+        date.year,
+        date.month,
+        date.hour,
+        date.minute,
+        date.second,
+        ".tgz",
+    )
+    fullpath = "version/", filename
+    os.system("mkdir -p versions && tar -czvf {} {}".format(fullpath, "web_static"))
+    return os.path.abspath(filename[0])
