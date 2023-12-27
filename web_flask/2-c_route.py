df8d77b93f3a6e7eff3190b89446212d597fe37c
hepona <hepona10@gmail.com>
2023-12-27 13:01:45 +0100

    0 1 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is(text):
+    return "C {}".format(text)
+
+
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
