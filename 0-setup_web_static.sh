74ab368f97bf044b891e759bed47ba34a096c332
hepona <hepona10@gmail.com>
2024-02-01 18:44:28 +0100

    0 finish ?

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static.
+
+#sh script that sets up your web servers for the deployment of web_static.
 sudo apt update
 
 # Install Nginx
@@ -13,22 +14,20 @@ sudo mkdir -p /data/web_static/releases/test
 # # Create an index.html file
 echo "<html>
   <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >/data/web_static/releases/test/index.html
+    </head>
+      <body>
+          Holberton School
+            </body>
+            </html>" >/data/web_static/releases/test/index.html
 
-# # Create a symbolic link
+            # # Create a symbolic link
 
-rm -rf /data/web_static/current
-ln -s /data/web_static/releases/test/ /data/web_static/current
-# # Adjust permissions
-sudo chown -R ubuntu:ubuntu /data/
+            rm -rf /data/web_static/current
+            ln -s /data/web_static/releases/test/ /data/web_static/current
+            # # Adjust permissions
+            sudo chown -R ubuntu:ubuntu /data/
 
-# Configure Nginx
-# sudo sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-
+            # Configure Nginx
 CONFIG="
 server {
     location /hbnb_static/ {
