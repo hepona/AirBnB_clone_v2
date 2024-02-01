d30f7e0ef3878f54adadcd22f513be1414933101
hepona <hepona10@gmail.com>
2024-02-01 19:42:38 +0100

    0 fix symbolic link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,10 @@
 #bash script that sets up your web servers for the deployment of web_static.
 
 sudo apt update
-
-# Install Nginx
 sudo apt install nginx
-
-# Create necessary directories
 sudo mkdir -p /data/web_static/releases
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
-
-#Create an index.html file
 echo "<html>
   <head>
     </head>
@@ -19,15 +13,9 @@ echo "<html>
           Holberton School
             </body>
             </html>" >/data/web_static/releases/test/index.html
-
-# # Create a symbolic link
-
 rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
-# # Adjust permissions
 sudo chown -R ubuntu:ubuntu /data/
-
-# Configure Nginx
 CONFIG="
 server {
     location /hbnb_static/ {
@@ -36,5 +24,4 @@ server {
 }
 "
 echo "$CONFIG" | sudo tee /etc/nginx/sites-available/default
-# Restart Nginx
 sudo service nginx restart
