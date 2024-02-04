d91f57f505f80ea7af45ab8d3baa9bc818df2100
hepona <hepona10@gmail.com>
2024-02-05 00:22:30 +0100

    101

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,47 @@
+#task 0 in puppet
+package {'apt':
+ensure => 'installed',
+}
+exec { 'apt-update':
+command => '/usr/bin/apt update',
+}
+exec { 'install_nginx':
+command => '/usr/bin/apt install -y nginx',
+}
+#sudo mkdir -p /data/web_static/{releases/test,shared}
+file {'/data/web_static/releases/test, /data/web_static/shared}':
+ensure => directory,
+recurse => true,
+}
+file {'/data/web_static/releases/test/index.html':
+content => "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>",
+}
+
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test',
+}
+
+file {'/data':
+
+ensure => directory,
+recurse => true,
+owner   => 'ubuntu',
+group   => 'ubuntu',
+}
+file {'/etc/nginx/sites-available/default':
+content => "server {
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+    }
+}",
+}
+exec { 'restart_nginx':
+command => '/usr/sbin/nginx service nginx restart',
+}
