e0b90b3be62fccef0eecd600fd49c00ff97d9eeb
hepona <hepona10@gmail.com>
2024-02-04 17:22:41 +0100

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,40 +6,22 @@ sudo apt install nginx
 sudo mkdir -p /data/web_static/releases
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
-echo "<html>
+echo "
+<html>
   <head>
-    </head>
-      <body>
-          Holberton School
-            </body>
-            </html>" >/data/web_static/releases/test/index.html
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" >/data/web_static/releases/test/index.html
 rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 CONFIG="
 server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
     location /hbnb_static/ {
         alias /data/web_static/current/;
-    }
-
-    error_page 404 /custom_404.html;
-    location = /custom_404.html {
-        root /usr/share/nginx/html;
-        internal;
-    }
-
-    error_page 500 502 503 504 /custom_50x.html;
-    location = /custom_50x.html {
-        root /usr/share/nginx/html;
-        internal;
     }
 }
 
