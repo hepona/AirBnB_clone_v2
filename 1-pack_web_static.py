3ae23fb52d37a1b36ee8eafbe25db1f32755da0c
hepona <hepona10@gmail.com>
2024-02-04 22:36:04 +0100

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,19 +11,12 @@ def do_pack():
     """generate .tgz file"""
     date = datetime.datetime.now()
     filename = (
-        "web_static_",
-        date.year,
-        date.month,
-        date.hour,
-        date.minute,
-        date.second,
-        ".tgz",
+        f"web_static_{date.year}{date.month}{date.hour}{date.minute}{date.second}.tgz"
     )
-    fullpath = "version/", filename
-    print("Packing web_static to {}".format(fullpath))
 
-    r = local(
-        "mkdir -p versions && tar -czvf {} {}".format(fullpath, "web_static"))
+    fullpath = "versions/" + filename
+    print("Packing web_static to {}".format(fullpath))
+    r = local("mkdir -p versions && tar -czvf {} {}".format(fullpath, "web_static"))
     if r == 0:
         return os.path.abspath(filename[0])
     else:
