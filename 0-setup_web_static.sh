3f2f38a3be772e06451b3e11539fea66a9c031a7
hepona <hepona10@gmail.com>
2024-02-04 18:13:00 +0100

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-#bash script that sets up your web servers for the deployment of web_static.
-
+# Bash script that sets up web servers for the deployment of web_static.
 sudo apt update
-sudo apt install nginx
-sudo mkdir -p /data/web_static/releases
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases/test
+sudo apt install -y nginx
+
+sudo mkdir -p /data/web_static/{releases/test,shared}
 echo "
 <html>
   <head>
@@ -14,9 +12,9 @@ echo "
     Holberton School
   </body>
 </html>
-" >/data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
-ln -s /data/web_static/releases/test/ /data/web_static/current
+" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
 CONFIG="
 server {
@@ -24,7 +22,7 @@ server {
         alias /data/web_static/current/;
     }
 }
-
 "
+
 echo "$CONFIG" | sudo tee /etc/nginx/sites-available/default
 sudo service nginx restart
