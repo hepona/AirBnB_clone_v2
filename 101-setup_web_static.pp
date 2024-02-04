be7378b67673a93cef4deefe21594b78d6343b51
hepona <hepona10@gmail.com>
2024-02-05 00:40:54 +0100

    101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,33 +1,33 @@
 #task 0 in puppet
 package {'apt':
-ensure => 'installed',
+  ensure => 'installed',
 }
+
 exec { 'apt-update':
-command => '/usr/bin/apt update',
+  command => '/usr/bin/apt update',
 }
+
 exec { 'install_nginx':
-command => '/usr/bin/apt install -y nginx',
+  command => '/usr/bin/apt install -y nginx',
 }
 
-file {'/data':
+file { '/data/web_static/releases/test':
+  ensure => directory,
+  recurse => true,
+}
 
-ensure => directory,
-recurse => true,
-owner   => 'ubuntu',
-group   => 'ubuntu',
+file { '/data/web_static/shared':
+  ensure => directory,
 }
-file {'/data/web_static/releases/test' , '/data/web_static/shared':
-ensure => directory,
-recurse => true,
-}
-file {'/data/web_static/releases/test/index.html':
-content => "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>",
+
+file { '/data/web_static/releases/test/index.html':
+  content => "<html>
+    <head>
+    </head>
+    <body>
+      Holberton School
+    </body>
+  </html>",
 }
 
 file { '/data/web_static/current':
@@ -35,13 +35,21 @@ file { '/data/web_static/current':
   target => '/data/web_static/releases/test',
 }
 
-file {'/etc/nginx/sites-available/default':
-content => "server {
+file { '/data':
+  ensure => directory,
+  recurse => true,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/etc/nginx/sites-available/default':
+  content => "server {
     location /hbnb_static/ {
         alias /data/web_static/current/;
     }
 }",
 }
+
 exec { 'restart_nginx':
-command => '/usr/sbin/service nginx restart',
+  command => '/usr/sbin/service nginx restart',
 }
